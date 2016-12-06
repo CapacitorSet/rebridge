19ab916504f50ee64bd58ef99ba9494d1b28d4bc
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-12-06 11:42:17 +0100

    Woooo, added support for every Array method 🎉

--- index.js
+++ index.js
@@ -2,7 +2,37 @@
 
 const assert = require("assert");
 
-// http://stackoverflow.com/a/18937118
+function promisableModify(rootKey, tree, fun) {
+	return new Promise((resolve, reject) => {
+		module.exports.redis.hget("rebridge", rootKey, (err, json) => {
+			if (err) return reject(err);
+			let rootValue = JSON.parse(json) || {};
+			if (rootValue === null) rootValue = {};
+			const ret = nestedApply(rootValue, tree, fun);
+			const newJson = JSON.stringify(rootValue);
+			module.exports.redis.hset("rebridge", rootKey, newJson, err => {
+				if (err) return reject(err);
+				resolve(ret);
+			});
+		});
+	});
+}
+
+// Abstracted version of http://stackoverflow.com/a/18937118.
+// Note: this _does not work_ with nested set
+function nestedApply(obj, path, fun) {
+	if (path.length === 0) return fun(obj);
+	// https://github.com/petkaantonov/bluebird/wiki/Optimization-killers#3-managing-arguments
+	let schema = obj;
+	const len = path.length;
+	for (let i = 0; i < len - 1; i++) {
+		const elem = path[i];
+		if (!schema[elem]) schema[elem] = {};
+		schema = schema[elem];
+	}
+	return fun(schema[path[len - 1]]);
+}
+
 function nestedSet(obj, path, value) {
 	assert.notStrictEqual(typeof value, "undefined");
 	let schema = obj; // a moving reference to internal objects within obj
@@ -15,47 +45,6 @@ function nestedSet(obj, path, value) {
 	}
 
 	return (schema[path[len - 1]] = value);
-}
-
-function nestedDelete(obj, path, prop) {
-	assert.notStrictEqual(typeof prop, "undefined");
-	let schema = obj; // a moving reference to internal objects within obj
-	const len = path.length;
-	// This can _not_ be refactored to for..of
-	for (let i = 0; i < len - 1; i++) {
-		const elem = path[i];
-		if (!schema[elem]) schema[elem] = {};
-		schema = schema[elem];
-	}
-
-	return (delete schema[path[len - 1]][prop]);
-}
-
-function nestedPush(obj, path, val) {
-	assert.notStrictEqual(typeof val, "undefined");
-	let schema = obj; // a moving reference to internal objects within obj
-	const len = path.length;
-	// This can _not_ be refactored to for..of
-	for (let i = 0; i < len - 1; i++) {
-		const elem = path[i];
-		if (!schema[elem]) schema[elem] = {};
-		schema = schema[elem];
-	}
-
-	return schema[path[len - 1]].push(val);
-}
-
-function nestedPop(obj, path) {
-	let schema = obj; // a moving reference to internal objects within obj
-	const len = path.length;
-	// This can _not_ be refactored to for..of
-	for (let i = 0; i < len - 1; i++) {
-		const elem = path[i];
-		if (!schema[elem]) schema[elem] = {};
-		schema = schema[elem];
-	}
-
-	return schema[path[len - 1]].pop();
 }
 
 /* Gets a "root value" from Redis (i.e. one stored in a Redis hash),
@@ -112,8 +101,8 @@ function ProxiedWrapper(promise, rootKey) {
 							const ret = (obj.tree.length > 0) ?
 								nestedSet(rootValue, obj.tree, val) :
 								(rootValue = val);
-							json = JSON.stringify(rootValue);
-							module.exports.redis.hset("rebridge", rootKey, json, err => {
+							const newJson = JSON.stringify(rootValue);
+							module.exports.redis.hset("rebridge", rootKey, newJson, err => {
 								if (err) return reject(err);
 								resolve(ret);
 							});
@@ -121,60 +110,37 @@ function ProxiedWrapper(promise, rootKey) {
 					});
 				}
 				if (key === "delete") {
-					return val => new Promise((resolve, reject) => {
-						module.exports.redis.hget("rebridge", rootKey, (err, json) => {
-							if (err) return reject(err);
-							let rootValue = JSON.parse(json);
-							if (rootValue === null) rootValue = {};
-							const ret = (obj.tree.length > 0) ?
-								nestedDelete(rootValue, obj.tree, val) :
-								(delete rootValue[val]);
-							json = JSON.stringify(rootValue);
-							module.exports.redis.hset("rebridge", rootKey, json, err => {
-								if (err) return reject(err);
-								resolve(ret);
-							});
-						});
-					});
+					return prop => promisableModify(rootKey, obj.tree, item => delete item[prop]);
 				}
-				if (key === "push") {
-					return val => new Promise((resolve, reject) => {
-						module.exports.redis.hget("rebridge", rootKey, (err, json) => {
-							if (err) return reject(err);
-							let rootValue = JSON.parse(json);
-							if (rootValue === null) rootValue = {};
-							const ret = (obj.tree.length > 0) ?
-								nestedPush(rootValue, obj.tree, val) :
-								rootValue.push(val);
-							json = JSON.stringify(rootValue);
-							module.exports.redis.hset("rebridge", rootKey, json, err => {
-								if (err) return reject(err);
-								resolve(ret);
-							});
-						});
-					});
-				}
-				if (key === "pop") {
-					return () => new Promise((resolve, reject) => {
-						module.exports.redis.hget("rebridge", rootKey, (err, json) => {
-							if (err) return reject(err);
-							let rootValue = JSON.parse(json);
-							if (rootValue === null) rootValue = {};
-							const ret = (obj.tree.length > 0) ?
-								nestedPop(rootValue, obj.tree) :
-								rootValue.pop();
-							json = JSON.stringify(rootValue);
-							module.exports.redis.hset("rebridge", rootKey, json, err => {
-								if (err) return reject(err);
-								resolve(ret);
-							});
-						});
-					});
-				}
-				if (key in Array.prototype) {
-					// Array methods
-					throw new Error(`Calling ${key} on Rebridge objects is not yet supported.`);
-				}
+				/*
+				This is complex, but rather elegant.
+				If the user is calling an Array method (eg. push), it returns a promise.
+				This promise walks the `rootKey` object using `obj.tree` as a path, and
+				applies the given function passing the same arguments.
+
+				    | Eg. if `key` is `"push"` and `rootKey` is
+				    |
+				    |     {
+					|         a: {
+					|             b: {
+					|                 c: [1]
+					|             }
+					|         }
+					|     }
+					|
+					| and `obj.tree` is `["a", "b", "c"]`, it will return a function
+					| that navigates the object until `a.b.c` (i.e. `[1]`), and will
+					| call
+					|
+					|     item => item["push"].apply(item, arguments)
+					|
+					| on that. So, when the function is actually executed (eg.
+					| `db.foo.a.b.c.push(10)`), it will call `item => item.push(10)`.
+				 */
+				if (key in Array.prototype)
+					return function() {
+						return promisableModify(rootKey, obj.tree, item => item[key].apply(item, arguments));
+					};
 				obj.tree.push(key);
 				return new ProxiedWrapper(obj, rootKey);
 			},
